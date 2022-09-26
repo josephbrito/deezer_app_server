50e0ee57d1b5f7848e92a67b17d7062228c664fb
= <=>
2022-09-26 14:44:41 -0300

    try fix bugs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
+require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
 const { typeDefs, resolvers } = require("./graphql");
 const { InMemoryLRUCache } = require("@apollo/utils.keyvaluecache");
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+} = require("apollo-server-core");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   cache: new InMemoryLRUCache(),
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
 server
-  .listen()
+  .listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => console.log(`Server runnig on port ${url}`))
   .catch(() => console.log("Error on connection"));
